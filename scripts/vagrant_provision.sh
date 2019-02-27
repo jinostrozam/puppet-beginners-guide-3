52f97bdf78f559287bf3aa08c1693fb5d05b4ad2
Jaime Inostroza <bladimirosky@gmail.com>
2019-02-27 00:26:44 -0300

    Modify files requiered for secret

--- scripts/vagrant_provision.sh
+++ scripts/vagrant_provision.sh
@@ -4,7 +4,8 @@ wget https://apt.puppetlabs.com/puppet-release-${DISTRIB_CODENAME}.deb
 dpkg -i puppet-release-${DISTRIB_CODENAME}.deb
 apt-get update
 apt-get -y install git puppet-agent
+sudo apt-get install git-core curl zlib1g-dev build-essential gcc libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev software-properties-common libffi-dev gnupg rng-tools
 echo 'Defaults        secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin"' >/etc/sudoers.d/puppet
-/opt/puppetlabs/puppet/bin/gem install gpgme --no-rdoc --no-ri
 /opt/puppetlabs/puppet/bin/gem install hiera-eyaml-gpg --no-rdoc --no-ri
 /opt/puppetlabs/puppet/bin/gem install r10k --no-rdoc --no-ri
+/opt/puppetlabs/puppet/bin/gem install gpgme --no-rdoc --no-ri 
